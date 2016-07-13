c3caab273cf3780ac5e125073a5bd4c3d8ab2c65
Karol <Karol.Skupien@smart4aviation.aero>
2016-07-14 00:29:31 +0200

    Travis trigger test. Make dummy test just to trigger travis CI tool.
    Change-Id: Ib1b8e29530fec2316b23ff57a1b682cfbebe2d47

---
+++ Server/src/test/TestTravisTrigger.java
@@ -0,0 +1,11 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestTravisTrigger {
+
+    @Test
+    public void testTravisTrigger() {
+        Assert.assertTrue(true);
+    }
+
+}
