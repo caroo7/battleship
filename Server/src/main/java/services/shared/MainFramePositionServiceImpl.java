f08b270bcd4c3c8e08c4276c739ce6a65b118ff9
Karol <karol_skupien@epam.com>
2016-08-05 14:35:40 +0200

    Use GraphicsDevice in MainFramePositionService cause Toolkit still makes problems when travis try to build

--- Server/src/main/java/services/shared/MainFramePositionServiceImpl.java
+++ Server/src/main/java/services/shared/MainFramePositionServiceImpl.java
@@ -5,17 +5,19 @@ import configuration.Config;
 import java.awt.*;
 
 public class MainFramePositionServiceImpl implements MainFramePositionService {
+
     private int frameCounter = 0;
 
-    private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    GraphicsDevice graphicsDevice = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 
-    private int widthGapBetweenFrames = (screenSize.width - 2 * Config.MAIN_FRAME_WIDTH) / 3;
+    private int widthGapBetweenFrames = (graphicsDevice.getDisplayMode().getWidth() - 2 * Config.MAIN_FRAME_WIDTH) / 3;
 
-    private int heightGapBetweenFrames = (screenSize.height - Config.MAIN_FRAME_HEIGHT) / 2;
+    private  int heightGapBetweenFrames =(graphicsDevice.getDisplayMode().getHeight()-Config.MAIN_FRAME_HEIGHT)/2;
 
     @Override
     public Point getProperFrameLocation() {
         frameCounter++;
         return frameCounter == 1 ? new Point(widthGapBetweenFrames, heightGapBetweenFrames) : new Point(2 * widthGapBetweenFrames + Config.MAIN_FRAME_WIDTH, heightGapBetweenFrames);
     }
+    
 }
