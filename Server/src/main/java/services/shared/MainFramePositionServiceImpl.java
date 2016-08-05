26b54e3d2b6f9d6c39136c5ce8961cc6ba6a0491
Karol <karol_skupien@epam.com>
2016-08-05 14:06:40 +0200

    Resolve build error due to Toolkit class usage

--- Server/src/main/java/services/shared/MainFramePositionServiceImpl.java
+++ Server/src/main/java/services/shared/MainFramePositionServiceImpl.java
@@ -7,11 +7,13 @@ import java.awt.*;
 public class MainFramePositionServiceImpl implements MainFramePositionService {
     private int frameCounter = 0;
 
-    private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    //private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
-    private int widthGapBetweenFrames = (screenSize.width - 2 * Config.MAIN_FRAME_WIDTH) / 3;
+    GraphicsDevice graphicsDevice = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 
-    private  int heightGapBetweenFrames =(screenSize.height-Config.MAIN_FRAME_HEIGHT)/2;
+    private int widthGapBetweenFrames = (graphicsDevice.getDisplayMode().getWidth() - 2 * Config.MAIN_FRAME_WIDTH) / 3;
+
+    private  int heightGapBetweenFrames =(graphicsDevice.getDisplayMode().getHeight()-Config.MAIN_FRAME_HEIGHT)/2;
 
     @Override
     public Point getProperFrameLocation() {
